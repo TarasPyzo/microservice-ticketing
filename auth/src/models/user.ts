f3694014be8734aeb15736e54dc1e894933dc9d3
TarasPyzo <tyro.asap@gmail.com>
2021-10-10 00:36:45 +0300

    Format JSON response in Auth Service

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -10,6 +10,13 @@ interface User {
 const userSchema = new mongoose.Schema<User>({
   email: { type: String, required: true },
   password: { type: String, required: true },
+}, {
+  toJSON: {
+    transform(doc, ret) {
+      delete ret.password;
+      delete ret.__v;
+    }
+  },
 });
 
 userSchema.pre('save', async function(done) {
